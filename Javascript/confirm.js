dbeeb61ea5883a8f2355e5bb96a788e02d9a2448
Antonio Barrafato <Antonio.Barrafato18@alumni.loyola.ca>
2017-04-03 13:37:30 -0400

    just got evaluted
    eveything works, was able to change the header when logining in to make
    it the username, i was also able to format it a little bigger. all the
    comments are up to date.

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -20,20 +20,27 @@ function checkTime(i) {
     return i;
 }
 
+/** this is what makes the login work **/
 function login(){
+	/** these are the variables **/
 	var usernamee = "AntonioB"
 	var passwordd = "antonio918"
 	var user = prompt("please enter your username!");
 	var pass = prompt("please enter your password!");
-	
+	/** this just says if the username and password match the variable, it tells 
+		the user welcome and displays your username at the top of thw page **/
 	if (user === usernamee){
 		if (pass === passwordd){
 			alert("Welcome "+ usernamee +" ")
 			document.getElementById("Usernamee").innerHTML = "Welcome" + " " + usernamee; 
-		}else{
+		}
+			else{
+		/** if the password id wrong it says wrong password **/
 			alert("WRONG PASSWORD");
 		}
-	}else{
-		alert("WRONG USERNAME");
+		}	else{
+		/** if the username is wrong, it says wrong username **/
+			alert("WRONG USERNAME");
 		}
 }
+
