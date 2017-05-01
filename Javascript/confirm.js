d9d2a7bbab4729b10c767abab362a33d63b7476c
Antonio Barrafato <Antonio.Barrafato18@alumni.loyola.ca>
2017-05-01 13:37:55 -0400

    got the for loop to work

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -23,23 +23,17 @@ function checkTime(i) {
 /** this is what makes the login work **/
 function login(){
 	/** these are the variables **/
-	var Arrayuser = ["user1", "user2", "user3", "AntonioB"]
-	var Arraypass = ["pass1", "pass2", "pass3", "antonio918"]
+	var userarray = ["user1", "user2", "user3", "AntonioB"]
+	var passarray = ["pass1", "pass2", "pass3", "antonio918"]
 	var user = prompt("please enter your username!");
 	var pass = prompt("please enter your password!");
 	/** this just says if the username and password match the variable, it tells 
-		the user welcome and displays your username at the top of thw page **/
+		the user welcome and displays your username at the top of the page **/
 	
-	for (var i = 0; i < Arrayuser.length; i++ ){
-		if((user === Arrayuser[i]) & (pass === Arraypass[i])){			
-			alert("Welcome "+ usernamee +" ")
-			document.getElementById("Usernamee").innerHTML = "Welcome" + " " + usernamee; 
-		else{
-		/** if the password id wrong it says wrong password **/
-			alert("WRONG PASSWORD");
-		}else{
-		/** if the username is wrong, it says wrong username **/
-			alert("WRONG USERNAME");
+	for (i = 0; i <= userarray.length; i++){
+		if (user === userarray[i] && pass === passarray[i]){
+			alert("Welcome "+ userarray[i] +" ");
+			document.getElementById("userarray").innerHTML = "Welcome "+ userarray[i];  
 		}
 	}
 }
