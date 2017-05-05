4a623ae0fee63bb50288a35389a6bdfe8ddf03d6
Antonio Barrafato <Antonio.Barrafato18@alumni.loyola.ca>
2017-05-05 14:53:07 -0400

    made a trickier login

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -1,17 +1,21 @@
 /** this is what makes the login work **/
 function login(){
 	/** these are the variables **/
-	var userarray = ["user1", "user2", "user3", "AntonioB"]
-	var passarray = ["pass1", "pass2", "pass3", "antonio918"]
+	var userarray = ["user1", "user2", "user3", "AntonioB", "Sacha"]
+	var passarray = ["pass1", "pass2", "pass3", "antonio918", "Mancuso"]
 	var user = prompt("please enter your username!");
 	var pass = prompt("please enter your password!");
-	/** this just says if the username and password match the variable, it tells 
-		the user welcome and displays your username at the top of the page **/
 	
-	for (i = 0; i <= userarray.length; i++){
-		if (user === userarray[i] && pass === passarray[i]){
-			alert("Welcome "+ userarray[i] +" ");
-			document.getElementById("userarray").innerHTML = "Welcome "+ userarray[i];  
+	for (a = 0; a <= userarray.length; a++){
+		/* the for loop searches the userarray and passarray, and if they match, 
+it goes on with the code*/
+		if (user === userarray[a] && pass === passarray[a]){
+			alert("Welcome "+ userarray[a]);
+		/** this just says if the username and password match the variable, it tells 
+the user welcome and displays your username at the top of the page **/
+			document.getElementById("userarray").innerHTML = "Welcome "+ userarray[a];  
+			console.log("Username: " + userarray[a])
+			console.log("Password: " + passarray[a])
 		}
 	}
 }
