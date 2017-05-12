f489e2071a7dd5a7c87aa79d6cedad5cbcb418c3
Antonio Barrafato <Antonio.Barrafato18@alumni.loyola.ca>
2017-05-12 14:00:52 -0400

    MADE THE ELSE STaTeMENT

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -7,16 +7,29 @@ function login(){
 	var user = prompt("please enter your username!");
 	var pass = prompt("please enter your password!");
 	
+	if (login = true){
 	for (a = 0; a <= userarray.length; a++){
 		/* the for loop searches the userarray and passarray, and if they match, 
 it goes on with the code*/
 		if (user === userarray[a] && pass === passarray[a]){
 			alert("Welcome "+ userarray[a]);
+			valid = true;
 		/** this just says if the username and password match the variable, it tells 
 the user welcome and displays your username at the top of the page **/
-			document.getElementById("userarray").innerHTML = "Welcome "+ userarray[a];  
+			/* this logs the username and password to the console*/
 			console.log("Username: " + userarray[a])
 			console.log("Password: " + passarray[a])
+			break;
+		}else{
+			valid = false;
 		}
+	}
+		if (valid === true) {
+        document.getElementById("userarray").innerHTML = "Welcome" + " " + userarray[a];
+		}
+		else {
+        alert("Incorrect username or password");
+		}
+		
 	}
 }
